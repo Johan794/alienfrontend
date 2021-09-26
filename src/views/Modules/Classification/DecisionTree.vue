30485ab4999a56f1a455ba3600d6646c9476881d
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2021-09-26 15:14:20 -0500

    first sample of the view

--- src/views/Modules/Classification/DecisionTree.vue
+++ src/views/Modules/Classification/DecisionTree.vue
@@ -12,10 +12,14 @@
         <div>
             <h1 class="text-center"> DECISION TREE </h1>
     </div>
+    <!--
+        Esta es la parte de los dos botones de la izquierda
+    -->
     <div class="col-lg-12" align="center">
         <div class="text-left">
             <el-tooltip
             content="Upload your own dataset" effect="light" :open-delay="300" placement="top">
+            <el-input type="file" class="col-md-5 my-1" outlined id="external"></el-input>
             <base-button type="primary" @click.native="modals.notice = true">Submit yours</base-button>
             </el-tooltip>
         </div>
@@ -23,12 +27,16 @@
         <div class="text-left">
             <el-tooltip
             content="Upload the given dataset" effect="light" :open-delay="300" placement="top">
+            <el-input type="file" class="col-md-5 my-1" outlined id="predefined"></el-input>
             <base-button type="primary" @click.native="modals.notice = true">Submit predefined</base-button>
             </el-tooltip>
         </div>
       <form>
         <card>
           <div>
+              <!--
+                  Aquí esta la parte derecha donde esta el texto y los cuadros para llenar
+              -->
             <div class="row" align="right">
               <label class="col-sm-2 col-form-label" style="font-size: 0.05 rem">N# Folds</label>
 
@@ -40,7 +48,37 @@
                   placement="top"
                 >
                   <base-input
-                    v-model="title"
+                    v-model="title" placeholder="0"
+                    :error="getError('required')"
+                  ></base-input>
+                </el-tooltip>
+
+                <label class="col-sm-2 col-form-label" style="font-size: 0.05 rem">Max Depth</label>
+
+              <div class="col-sm-8">
+                <el-tooltip
+                  content="Type the max depth"
+                  effect="light"
+                  :open-delay="300"
+                  placement="top"
+                >
+                  <base-input
+                    v-model="title" placeholder="0"
+                    :error="getError('required')"
+                  ></base-input>
+                </el-tooltip>
+
+                <label class="col-sm-2 col-form-label" style="font-size: 0.05 rem">Min size</label>
+
+              <div class="col-sm-8">
+                <el-tooltip
+                  content="Type the min size"
+                  effect="light"
+                  :open-delay="300"
+                  placement="top"
+                >
+                  <base-input
+                    v-model="title" placeholder="0"
                     :error="getError('required')"
                   ></base-input>
                 </el-tooltip>
@@ -51,6 +89,9 @@
         </card>
       </form>
     </div>
+    <!--
+        Aquí esta la parte del grafo, falta colocar el texto allí, pero no se como quede mejor
+    -->
     <div id="mygraph"></div>
     <input class="col-sm-12 btn btn-outline-danger expand" type="button" v-on:click="nextStep" value="Next">
 </template>
