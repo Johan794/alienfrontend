5d828307edfc0c6e676e1f1562b86a442f8bf34f
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2022-03-31 21:42:46 -0500

    Modifing the view of the data information

--- src/views/Modules/Classification/DecisionTree.vue
+++ src/views/Modules/Classification/DecisionTree.vue
@@ -8,11 +8,41 @@
 .tree {
   height: 800px;
 }
-#container{
-   width: 400;
-   height: 400px;
-   display: flex;
-   align-items: center;
+#container {
+  width: 400;
+  height: 400px;
+  display: flex;
+  align-items: center;
+}
+
+.event-selected {
+  width: 400;
+  height: 200px;
+  border: 2px solid lightgray;
+  margin: 50px 500px;
+}
+
+.heading-event {
+  margin: 20px;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: gainsboro;
+}
+
+.information-event {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.label-data {
+  margin: 10px;
+}
+
+input {
+  text-align: center;
 }
 </style>
 <script
@@ -20,168 +50,241 @@
   src="https://visjs.github.io/vis-network/standalone/umd/vis-network.min.js"
 ></script>
 <template>
-<div>
   <div>
-    <div class="col-12" align="center">
-      <h1> DECISION TREE </h1>
-    </div>
-    <div class="col-12" align="center">
-      <el-tooltip
-        content="Know more about decision trees"
-        effect="light"
-        :open-delay="300"
-        placement="top"
-      >
-        <base-button type="primary" @click.native="modals.notice = true"> About Decision Tree </base-button>
-      </el-tooltip>
-    
-      <modal :show.sync="modals.notice" footerClasses="justify-content-center" type="notice">
-        <template slot="header">
-         <h5 slot="header" class="title">What is a decision tree? </h5>
-        </template>
-        <div class="instruction">
-        <div class="row">
-          <strong>
-            <i class="tim-icons icon-book-bookmark text-primary"></i>What is it?
-          </strong>
-          <p class="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dicta obcaecati at,
-            earum eius possimus voluptates dolorem incidunt velit illum ad repellat, magnam quisquam
-            aliquid tenetur eaque recusandae modi excepturi.
-          </p>
-        </div>
-        </div>
-        <div class="instruction">
-        <div class="row">
-          <strong>
-            <i class="tim-icons icon-notes text-info"></i>These are its main features
-          </strong>
-          <p class="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dicta obcaecati at,
-            earum eius possimus voluptates dolorem incidunt velit illum ad repellat, magnam quisquam
-            aliquid tenetur eaque recusandae modi excepturi.
-          </p>
-        </div>
-        </div>
-        <div class="instruction">
-        <div class="row">
-          <strong>
-            <i class="tim-icons icon-paper text-success"></i>What kind of trees can we find?
-          </strong>
-          <p class="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit dicta obcaecati at,
-            earum eius possimus voluptates dolorem incidunt velit illum ad repellat, magnam quisquam
-            aliquid tenetur eaque recusandae modi excepturi.
-          </p>
-        </div>
-        </div>
-        <div slot="footer" class="justify-content-center">
-          <base-button type="primary" round @click.native="modals.notice = false">Finish</base-button>
-        </div>
-      </modal>
-    </div>
-    <br>
-    <div class="col-lg-12" align="center">
-        <div class="text-left">
-            <el-tooltip
-            content="Upload your own dataset" effect="light" :open-delay="300" placement="top">
-            <el-input type="file" class="col-md-5 my-1" outlined id="external"></el-input>
-            <base-button type="primary" @click.native="modals.notice = true" style="width:500px;">Submit yours</base-button>
-            </el-tooltip>
-            <label class="col-sm-2 col-form-label" style="font-size: 0.05 rem" align="right">N# Folds</label>
-            <el-tooltip
-                  content="Type the number of folds"
-                  effect="light"
-                  :open-delay="300"
-                  placement="top"
-                >
-              <input v-model="message" placeholder="0">
-            </el-tooltip>
-            <br />
-            <el-tooltip
-            content="Upload the given dataset" effect="light" :open-delay="300" placement="top">
-            <el-input type="file" class="col-md-5 my-1" outlined id="predefined"></el-input>
-            <base-button type="primary" @click.native="modals.notice = true" style="width:500px;">Submit predefined</base-button>
-            </el-tooltip>
-            
-        </div>
-        </div>
-        <br />
-        <div class="text-left">
-            
-        </div>
-        <div align="right">
-          <div class="col-sm-8">
-            
-          </div>
-        </div>
-        <div align="right">
-            <div class="col-sm-8">
-              <label class="col-sm-2 col-form-label" style="font-size: 0.05 rem" align="right">Max Depth</label>
-              <el-tooltip
-                  content="Type the max depth"
-                  effect="light"
-                  :open-delay="300"
-                  placement="top"
-                >
-              <input v-model="m2" placeholder="0">
-              </el-tooltip>
-            </div>
-        </div>
-        <div align="right">
-          <div class="col-sm-8">
-              <label class="col-sm-2 col-form-label" style="font-size: 0.05 rem" align="right">Min size</label>
-              <el-tooltip
-                  content="Type the minimum size"
-                  effect="light"
-                  :open-delay="300"
-                  placement="top"
-                >
-              <input v-model="m3" placeholder="0">
-            </el-tooltip>
-          </div>
-        </div>
-        <div id="mygraph">
-        <input class="col-sm-12 btn btn-outline-danger expand" type="button" v-on:click="nextStep" value="Next">
-        <div id="container"> 
-         
-       <tree :data="treeData"
-         node-text="name" 
-         duration="750" 
-         layoutType="vertical" 
-         :marginX = "30" 
-         :marginY = "30"
-         :radius = "10"
-         :type = "tree"
-         :nodeTextMargin = "6"
-         :leafTextMargin = " 6 "
-         :zoomable = "true"
-         class="tree"
-         >
-         </tree>
+    <div>
+      <div class="col-12" align="center">
+        <h1>DECISION TREE</h1>
       </div>
+      <div class="col-12" align="center">
+        <el-tooltip
+          content="Know more about decision trees"
+          effect="light"
+          :open-delay="300"
+          placement="top"
+        >
+          <base-button type="primary" @click.native="modals.notice = true">
+            About Decision Tree
+          </base-button>
+        </el-tooltip>
+
+        <modal
+          :show.sync="modals.notice"
+          footerClasses="justify-content-center"
+          type="notice"
+        >
+          <template slot="header">
+            <h5 slot="header" class="title">What is a decision tree?</h5>
+          </template>
+          <div class="instruction">
+            <div class="row">
+              <strong>
+                <i class="tim-icons icon-book-bookmark text-primary"></i>What is
+                it?
+              </strong>
+              <p class="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit dicta obcaecati at, earum eius possimus voluptates
+                dolorem incidunt velit illum ad repellat, magnam quisquam
+                aliquid tenetur eaque recusandae modi excepturi.
+              </p>
+            </div>
+          </div>
+          <div class="instruction">
+            <div class="row">
+              <strong>
+                <i class="tim-icons icon-notes text-info"></i>These are its main
+                features
+              </strong>
+              <p class="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit dicta obcaecati at, earum eius possimus voluptates
+                dolorem incidunt velit illum ad repellat, magnam quisquam
+                aliquid tenetur eaque recusandae modi excepturi.
+              </p>
+            </div>
+          </div>
+          <div class="instruction">
+            <div class="row">
+              <strong>
+                <i class="tim-icons icon-paper text-success"></i>What kind of
+                trees can we find?
+              </strong>
+              <p class="description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit dicta obcaecati at, earum eius possimus voluptates
+                dolorem incidunt velit illum ad repellat, magnam quisquam
+                aliquid tenetur eaque recusandae modi excepturi.
+              </p>
+            </div>
+          </div>
+          <div slot="footer" class="justify-content-center">
+            <base-button
+              type="primary"
+              round
+              @click.native="modals.notice = false"
+              >Finish</base-button
+            >
+          </div>
+        </modal>
+      </div>
+      <br />
+      <div class="col-lg-12" align="center">
+        <div class="text-left">
+          <el-tooltip
+            content="Upload your own dataset"
+            effect="light"
+            :open-delay="300"
+            placement="top"
+          >
+            <el-input
+              type="file"
+              class="col-md-5 my-1"
+              outlined
+              id="external"
+            ></el-input>
+            <base-button
+              type="primary"
+              @click.native="modals.notice = true"
+              style="width: 500px"
+              >Submit yours</base-button
+            >
+          </el-tooltip>
+          <label
+            class="col-sm-2 col-form-label"
+            style="font-size: 0.05 rem"
+            align="right"
+            >N# Folds</label
+          >
+          <el-tooltip
+            content="Type the number of folds"
+            effect="light"
+            :open-delay="300"
+            placement="top"
+          >
+            <input v-model="message" placeholder="0" />
+          </el-tooltip>
+          <br />
+          <el-tooltip
+            content="Upload the given dataset"
+            effect="light"
+            :open-delay="300"
+            placement="top"
+          >
+            <el-input
+              type="file"
+              class="col-md-5 my-1"
+              outlined
+              id="predefined"
+            ></el-input>
+            <base-button
+              type="primary"
+              @click.native="modals.notice = true"
+              style="width: 500px"
+              >Submit predefined</base-button
+            >
+          </el-tooltip>
         </div>
+      </div>
+      <br />
+      <div class="text-left"></div>
+      <div align="right">
+        <div class="col-sm-8"></div>
+      </div>
+      <div align="right">
+        <div class="col-sm-8">
+          <label
+            class="col-sm-2 col-form-label"
+            style="font-size: 0.05 rem"
+            align="right"
+            >Max Depth</label
+          >
+          <el-tooltip
+            content="Type the max depth"
+            effect="light"
+            :open-delay="300"
+            placement="top"
+          >
+            <input v-model="m2" placeholder="0" />
+          </el-tooltip>
+        </div>
+      </div>
+      <div align="right">
+        <div class="col-sm-8">
+          <label
+            class="col-sm-2 col-form-label"
+            style="font-size: 0.05 rem"
+            align="right"
+            >Min size</label
+          >
+          <el-tooltip
+            content="Type the minimum size"
+            effect="light"
+            :open-delay="300"
+            placement="top"
+          >
+            <input v-model="m3" placeholder="0" />
+          </el-tooltip>
+        </div>
+      </div>
+      <div id="mygraph">
+        <input
+          class="col-sm-12 btn btn-outline-danger expand"
+          type="button"
+          v-on:click="nextStep"
+          value="Next"
+        />
+        <div id="container">
+          <tree
+            :data="treeData"
+            node-text="name"
+            duration="750"
+            layoutType="vertical"
+            :marginX="30"
+            :marginY="30"
+            :radius="10"
+            :type="tree"
+            :nodeTextMargin="6"
+            :leafTextMargin="6"
+            :zoomable="true"
+            class="tree"
+          >
+          </tree>
+        </div>
+      </div>
+      <div class="event-selected">
+        <div class="heading-event">Información del nodo seleccionado</div>
+        <div class="information-event">
+          <p class="label-data">Nombre del nodo:</p>
+          <form>
+            <input type="text" placeholder="Informacion" />
+          </form>
+        </div>
+        <div class="information-event">
+          <p class="label-data">Información del nodo:</p>
+          <form>
+            <input type="text" placeholder="Data" />
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { Modal, BaseAlert } from "src/components";
-import { tree } from 'vued3tree'
-import  Data  from 'src/views/Modules/Classification/tree.json'
+import { tree } from "vued3tree";
+import Data from "src/views/Modules/Classification/tree.json";
 export default {
-
   components: {
     tree,
     Modal,
     BaseAlert,
-    Data
-
+    Data,
   },
 
   data() {
     return {
-       treeData: Data,
+      treeData: Data,
       /*
        treeData: {
 
@@ -199,29 +302,25 @@ export default {
         },
         */
 
-        modals: {
-         notice: false  
-         /*
+      modals: {
+        notice: false,
+        /*
          sq: false,
          ss: false,
          rs: false
          */
-        }
-        
-        
-        
+      },
     };
   },
 
   methods: {
-    metodo1() {
-    },
-  }
+    metodo1() {},
+  },
 };
 </script>
 
 <style>
-button{
-  min-width:100px
+button {
+  min-width: 100px;
 }
 </style>
